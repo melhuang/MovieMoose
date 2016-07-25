443269b8c939774d8ae1aa918c921e9a4ae6e18f
Melissa Huang <melissahuang10@gmail.com>
2016-07-24 18:22:50 -0700

    View Holder and clean up

--- app/src/main/java/com/codepath/moviemoose/MoviesAdapter.java
+++ app/src/main/java/com/codepath/moviemoose/MoviesAdapter.java
@@ -21,9 +21,17 @@ import java.util.List;
  */
 public class MoviesAdapter extends ArrayAdapter<Movie> {
 
+    // URL constants
     final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p";
     final String POSTER_WIDTH = "/w154";
     final String BACKDROP_WIDTH = "/w780";
+
+    // View Lookup cache
+    private static class ViewModel {
+        ImageView movieImageView;
+        TextView movieTitle;
+        TextView movieOverview;
+    }
 
     public MoviesAdapter(Context context, List<Movie> movies) {
         super(context, 0, movies);
@@ -34,30 +42,34 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
         // Get the movie for this position
         Movie movie = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
+        ViewModel viewModel; // view lookup cache stored in tag
         if (convertView == null) {
+            viewModel = new ViewModel();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_movie, parent, false);
+
+            viewModel.movieTitle = (TextView) convertView.findViewById(R.id.movieTitle);
+            viewModel.movieOverview = (TextView) convertView.findViewById(R.id.movieOverview);
+            viewModel.movieImageView = (ImageView) convertView.findViewById(R.id.movieImageView);
+
+            // Cache the viewModel, attach it to the view
+            convertView.setTag(viewModel);
+        } else {
+            // View is being recycled, inflate with view model
+            viewModel = (ViewModel) convertView.getTag();
         }
-        // Lookup view for data population
-        ImageView movieImageView = (ImageView) convertView.findViewById(R.id.movieImageView);
+
+
         int orientation = getContext().getResources().getConfiguration().orientation;
-        String image;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             String url = IMAGE_BASE_URL + POSTER_WIDTH + movie.poster_path;
-            Picasso.with(getContext()).load(url).into(movieImageView);
-//            Picasso.with(getContext()).load(movie.poster_path).resize(100, 0).into(movieImageView);
+            Picasso.with(getContext()).load(url).into(viewModel.movieImageView);
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             String url = IMAGE_BASE_URL + BACKDROP_WIDTH + movie.backdrop_path;
-            Picasso.with(getContext()).load(url).into(movieImageView);
-
+            Picasso.with(getContext()).load(url).into(viewModel.movieImageView);
         }
-
-
-        TextView movieTitle = (TextView) convertView.findViewById(R.id.movieTitle);
-        TextView movieOverview = (TextView) convertView.findViewById(R.id.movieOverview);
         // Populate the data into the template view using the data object
-
-        movieTitle.setText(movie.title);
-        movieOverview.setText(movie.overview);
+        viewModel.movieTitle.setText(movie.title);
+        viewModel.movieOverview.setText(movie.overview);
 
         // Return the completed view to render on screen
         return convertView;
